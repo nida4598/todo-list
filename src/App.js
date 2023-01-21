0efd5121e1850e03b3bd8df5fdb3f5b84edec542
nida4598 <nidaimtiaz4598@gmail.com>
2023-01-22 00:06:01 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './MyComponents/Header';
 import Footer from './MyComponents/Footer';
 import Todos from './MyComponents/Todos';
+import AddTodo from './MyComponents/AddTodo';
 import React, { useState } from 'react';
 
 function App() {
@@ -11,6 +12,17 @@ function App() {
     setTodos(todos.filter((e)=>{
       return e!=todo;
     }))
+  }
+  const addTodo =(title,desc)=> {
+    console.log("I am adding a todo", title,desc)
+    let sno= todos[todos.length-1].sno+1;
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc:desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
   }
   const [todos, setTodos] = useState([
     {
@@ -32,6 +44,7 @@ function App() {
   return (
     <>
     <Header title="To do List" subtitle="SEE WHAT YOU DO TODAY!!"/>
+    <AddTodo addTodo = {addTodo}/>
     <Todos todos={todos} onDelete={onDelete}/>
     <Footer/>
     </>
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -4,7 +4,7 @@ import TodoItems from "../MyComponents/TodoItems";
 const Todos = (props) => {
   return (
     <div className='container'>
-      <h3 className='text-center my-3'>
+      <h3 className='my-3'>
         Todos List
       </h3>
       {props.todos.length==0? <h3>No todos to display</h3>:
